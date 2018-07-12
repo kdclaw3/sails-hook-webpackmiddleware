46285de7cd1426dc4bb392f993b4a835a63417c7
Dee Clawson <kdclaw3@gmail.com>
2018-07-12 07:54:57 -0400

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+
+	'parser': 'babel-eslint',
+	'env': {
+		es6: true,
+		node: true,
+		browser: true
+	},
+	'parserOptions': {
+		ecmaVersion: 6,
+		sourceType: 'module',
+		allowImportExportEverywhere: false,
+		codeFrame: false,
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	'plugins': [
+		'react'
+	],
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'rules': {
+		//enable eslint rules
+		'array-bracket-spacing': ['warn', 'never'],
+		'comma-dangle': 1,
+		'comma-spacing': ['warn', { before: false, after: true }],
+		'eol-last': ['error', 'always'],
+		'eqeqeq': 'warn',
+		'indent': ['warn', 'tab'],
+		'jsx-quotes': ['warn', 'prefer-double'],
+		'key-spacing': ['warn', {beforeColon: false, afterColon: true}],
+		'no-multi-spaces': 'error',
+		'no-trailing-spaces': 'warn',
+		'semi': ['warn', 'always'],
+		'semi-spacing': ['warn', {before: false, after: false}],
+		'semi-style': ['warn', 'last'],
+		'quotes': ['warn', 'single', {allowTemplateLiterals: true}],
+		'quote-props': ['warn', 'consistent-as-needed', {keywords: true, numbers: true, unnecessary: true}],
+		'yoda': ['warn', 'never', { exceptRange: true }]
+
+		//override eslint eslint:recommended
+
+		//disable eslint eslint:recommended
+
+		//enable react rules
+
+		//override react react/recommended
+
+		//disable react react/recommended
+
+	},
+	'globals': {
+		sails: true,
+		User: true
+	}
+
+};
