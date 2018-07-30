01105fbff6bb267a8a09e360bd809fbe5a4187b7
Dee Clawson <kdclaw3@gmail.com>
2018-07-30 15:26:35 -0400

    v1.1.1
    -earthrocker linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,8 @@
 module.exports = {
 
-	'env': {
-		es6: true,
-		node: true,
-		browser: true
-	},
-	'parserOptions': {
-		ecmaVersion: 6,
-		sourceType: 'module',
-		allowImportExportEverywhere: false,
-		codeFrame: false,
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
 	'extends': [
-		'eslint:recommended',
+		'earthrocker'
 	],
-	'rules': {
-		//enable eslint rules
-		'array-bracket-spacing': ['warn', 'never'],
-		'comma-dangle': 1,
-		'comma-spacing': ['warn', { before: false, after: true }],
-		'eol-last': ['error', 'always'],
-		'eqeqeq': 'warn',
-		'indent': ['warn', 'tab'],
-		'jsx-quotes': ['warn', 'prefer-double'],
-		'key-spacing': ['warn', {beforeColon: false, afterColon: true}],
-		'no-multi-spaces': 'warn',
-		'no-trailing-spaces': 'warn',
-		'semi': ['warn', 'always'],
-		'semi-spacing': ['warn', {before: false, after: true}],
-		'semi-style': ['warn', 'last'],
-		'space-infix-ops': 'warn',
-		'quote-props': ['warn', 'consistent-as-needed', {keywords: true, numbers: true, unnecessary: true}],
-		'quotes': ['warn', 'single', {allowTemplateLiterals: true}],
-		'yoda': ['warn', 'never', { exceptRange: true }]
-
-		//override eslint eslint:recommended
-
-		//disable eslint eslint:recommended
-
-		//enable react rules
-
-		//override react react/recommended
-
-		//disable react react/recommended
-
-	},
 	'globals': {
 		sails: true
 	}
--- index.js
+++ index.js
@@ -4,104 +4,99 @@
 const webpack = require('webpack');
 
 
-
 /**
  * Webpack hook
  *
  * @description :: A hook to compile assets using Webhook.
  */
-module.exports = function defineWebpackHook(sails) {
+module.exports = function defineWebpackHook (sails) {
 
-	function log(s) {
-		sails.log.debug(`[sails-hook-webpackmiddleware] -> ${s}.`);
-	}
+  function log (s) {
+    sails.log.debug(`[sails-hook-webpackmiddleware] -> ${s}.`);
+  }
 
-	return {
+  return {
 
-		/**
+    /**
 		 * defaults
 		 *
 		 * The implicit configuration defaults merged into `sails.config` by this hook.
 		 *
 		 * @type {Dictionary}
 		 */
-		defaults: function () {
+    defaults: function () {
 
-			let defaults = {};
+      let defaults = {};
 
-			if (!(sails && sails.config && sails.config.webpackmiddleware && sails.config.webpackmiddleware.enabled)) {
+      if (!(sails && sails.config && sails.config.webpackmiddleware && sails.config.webpackmiddleware.enabled)) {
 
-				log(`DISABLED -> sails.config..webpackmiddleware !== true`);
+        log(`DISABLED -> sails.config..webpackmiddleware !== true`);
 
-			} else if (process.env.NODE_ENV !== 'production') {
+      } else if (process.env.NODE_ENV !== 'production') {
 
-				if (!sails.config['webpack' + process.env.NODE_ENV] && !sails.config.webpack) {
-					throw new Error(
-						'\n\n[sails-hook-webpackmiddleware] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n' +
+        if (!sails.config['webpack' + process.env.NODE_ENV] && !sails.config.webpack) {
+          throw new Error(
+            '\n\n[sails-hook-webpackmiddleware] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n' +
 						'[sails-hook-webpackmiddleware] No individual environment webpack, nor default webpack configuration found!\n' +
 						'[sails-hook-webpackmiddleware] -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-\n');
-				} else if (!sails.config['webpack' + process.env.NODE_ENV] && sails.config.webpack) {
-					log('No individual environment webpack config found, using default');
-				} else {
-					log('Using webpack' + process.env.NODE_ENV + ' configuration');
-				}
+        } else if (!sails.config['webpack' + process.env.NODE_ENV] && sails.config.webpack) {
+          log('No individual environment webpack config found, using default');
+        } else {
+          log('Using webpack' + process.env.NODE_ENV + ' configuration');
+        }
 
-			}
+      }
 
-			return defaults;
+      return defaults;
 
-		},
+    },
 
-		/**
+    /**
 		 * Runs when a Sails app loads/lifts.
 		 *
 		 * @param {Function} done
 		 */
-		initialize: function (done) {
+    initialize: function (done) {
 
-			if (!(sails && sails.config && sails.config.webpackmiddleware && sails.config.webpackmiddleware.enabled)) {
+      if (!(sails && sails.config && sails.config.webpackmiddleware && sails.config.webpackmiddleware.enabled)) {
 
-				return done();
+        return done();
 
-			} else if (process.env.NODE_ENV !== 'production') {
+      } else if (process.env.NODE_ENV !== 'production') {
 
-				log('initializing');
+        log('initializing');
 
-				sails.after('hook:http:loaded', function () {
+        sails.after('hook:http:loaded', function () {
 
-					//choose the config file to use
-					let configFile;
-					if (sails.config['webpack' + process.env.NODE_ENV]) {
-						configFile = sails.config['webpack' + process.env.NODE_ENV];
-					} else {
-						configFile = sails.config.webpack;
-					}
+          // choose the config file to use
+          let configFile;
+          if (sails.config['webpack' + process.env.NODE_ENV]) {
+            configFile = sails.config['webpack' + process.env.NODE_ENV];
+          } else {
+            configFile = sails.config.webpack;
+          }
 
-					const compiler = webpack(configFile);
-					const expressApp = sails.hooks.http.app;
+          const compiler = webpack(configFile);
+          const expressApp = sails.hooks.http.app;
 
-					log('webpack: webpack-dev-middleware');
-					expressApp.use(require('webpack-dev-middleware')(compiler, {
-						stats: {
-							colors: true
-						}
-					}));
+          log('webpack: webpack-dev-middleware');
+          expressApp.use(require('webpack-dev-middleware')(compiler, { stats: { colors: true } }));
 
-					log('webpack: webpack-hot-middleware');
-					expressApp.use(require('webpack-hot-middleware')(compiler, {
-						noInfo: true,
-						//quiet: true,
-						reload: true
-					}));
+          log('webpack: webpack-hot-middleware');
+          expressApp.use(require('webpack-hot-middleware')(compiler, {
+            noInfo: true,
+            // quiet: true,
+            reload: true
+          }));
 
-				});
+        });
 
-			}
-			// Continue lifting Sails.
-			return done();
+      }
+      // Continue lifting Sails.
+      return done();
 
-		}
+    }
 
-	};
+  };
 
 };
